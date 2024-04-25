960f84f4650e4d2dc4548a6bfef74697190aa947
Tim Mitra <tim@it-guy.com>
2024-04-25 12:00:39 -0400

    Initial Commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Tim Mitra on 2024-04-25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
